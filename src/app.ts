4b3113384f5e37fe85cd26787aaac69d1509bfe9
bahauddin fahad <bahauddin.fahad819@gmail.com>
2024-07-13 22:59:26 +0600

    -cors error solved

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://sporting-goods-client.vercel.app/',
-    ],
+    origin: '*',
   }),
 );
 
